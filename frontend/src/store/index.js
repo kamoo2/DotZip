52782cdb7effb3a96db2b086e6c4e07a9ce08c84
강수나 <acrnm148>
2022-11-24 15:19:56 +0900

    frontend,backend: QnA 추가(등록부분 수정필요)

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,11 +1,12 @@
-import Vue from 'vue';
-import Vuex from 'vuex';
-import { createVuexPersistedState } from 'vue-persistedstate';
+import Vue from "vue";
+import Vuex from "vuex";
+import { createVuexPersistedState } from "vue-persistedstate";
 
-import menu from './modules/menu';
-import user from './modules/user';
-import house from './modules/house';
-import bookmark from './modules/bookmark';
+import menu from "./modules/menu";
+import user from "./modules/user";
+import house from "./modules/house";
+import bookmark from "./modules/bookmark";
+import QnA from "./modules/QnA";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
@@ -17,11 +18,12 @@ export default new Vuex.Store({
     user,
     house,
     bookmark,
+    QnA
   },
   plugins: [
     createVuexPersistedState({
-      key: 'vuexStore',
-      storage: sessionStorage,
-    }),
-  ],
+      key: "vuexStore",
+      storage: sessionStorage
+    })
+  ]
 });
--- frontend/src/views/app/support/Contact.vue
+++ frontend/src/views/app/support/Contact.vue
@@ -8,8 +8,22 @@
     </b-row>
     <b-row>
       <b-colxx xxs="12">
-        <b-card class="mb-4" :title="$t('menu.contact')"> </b-card>
+        <b-card class="mb-4" :title="$t('menu.contact')">
+          <iframe
+            width="560"
+            height="315"
+            src="https://www.youtube.com/embed/an_0IDaPLw8"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen
+          ></iframe>
+        </b-card>
       </b-colxx>
     </b-row>
   </div>
 </template>
+
+<script>
+export default {};
+</script>
